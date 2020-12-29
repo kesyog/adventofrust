15249bdd83eb1c880b0a216bda7cd52837aca633
Kesavan Yogeswaran <kesyog@gmail.com>
2020-12-29 11:07:56 -0500

    Add solution template

---
+++ template.rs
@@ -0,0 +1,29 @@
+//! Solution to [AoC YEAR Day DAY](https://adventofcode.com/YEAR/day/DAY)
+
+fn part1() {
+}
+
+fn part2() {
+}
+
+fn main() {
+    let _input = include_str!("../inputs/dayDAY.txt");
+    println!("Part 1: {:?}", part1());
+    println!("Part 2: {:?}", part2());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = r#"
+    "#;
+
+    #[test]
+    fn given_part1_input() {
+    }
+
+    #[test]
+    fn given_part2_input() {
+    }
+}
